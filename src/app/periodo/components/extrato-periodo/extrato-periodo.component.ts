b2747e39003586f37f70f08e792bffa06ed6ce1b
luis.gomes <luis.gomes@objective.com.br>
2023-07-24 17:29:14 -0300

    creates the layout of the extrato-periodo component

--- src/app/periodo/components/extrato-periodo/extrato-periodo.component.ts
+++ src/app/periodo/components/extrato-periodo/extrato-periodo.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute } from '@angular/router';
+import { Periodo } from '../../domain/periodo';
+import { PeriodoService } from './../../service/periodo.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './extrato-periodo.component.html',
   styleUrls: ['./extrato-periodo.component.css'],
 })
-export class ExtratoPeriodoComponent {}
+export class ExtratoPeriodoComponent {
+  periodo?: Periodo;
+  constructor(
+    private route: ActivatedRoute,
+    private periodoService: PeriodoService
+  ) {}
+
+  ngOnInit(): void {
+    this.buscarPeriodo();
+  }
+
+  buscarPeriodo() {
+    let id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id)
+      this.periodoService
+        .buscarPeriodo(id)
+        .subscribe((periodo) => (this.periodo = periodo));
+  }
+}
