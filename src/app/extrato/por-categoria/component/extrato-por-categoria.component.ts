c1b521a95941e34e52384084b7f1e1073f510348
luis.gomes <luis.gomes@objective.com.br>
2023-11-01 08:56:34 -0300

    add valorTotal total value of expenses for extrato-por-categoria

--- src/app/extrato/por-categoria/component/extrato-por-categoria.component.ts
+++ src/app/extrato/por-categoria/component/extrato-por-categoria.component.ts
@@ -1,10 +1,10 @@
-import { Observable } from 'rxjs';
+import { ValorPorCategoria } from './../domain/valor-por-categoria';
 import { Component, Input } from '@angular/core';
 import { Periodo } from 'src/app/periodo/domain/periodo';
-import { ValorPorCategoria } from '../domain/valor-por-categoria';
 import { ExtratoPorCategoriaService } from '../service/extrato-por-categoria.service';
 import { Usuario } from 'src/app/usuario/domain/usuario';
 import { USUARIOS } from 'src/app/usuario/mock-usuario';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'extrato-individual',
@@ -15,35 +15,50 @@ export class ExtratoPorCategoriaComponent {
   @Input() periodo?: Periodo;
   valoresPorCategoria: ValorPorCategoria[] = [];
   usuarioLogado?: Usuario;
+  valorTotal?: number;
 
   constructor(private extratoPorCategoriaService: ExtratoPorCategoriaService) {}
 
   ngOnInit() {
     this.getUsuarioLogado();
-    this.getTotalPorCategoriaParaOUsuarioNoPeriodo();
-  }
-
-  getTotalPorCategoriaParaOUsuarioNoPeriodo() {
-    let usuarioId = this.getIdUsuarioLogado();
-    if (!this.periodo || !this.periodo.id)
-      throw new Error('Periodo não definido!');
-
-    this.extratoPorCategoriaService
-      .buscarValorTotalPorCategoriaEUsuarioNoPeriodo(this.periodo.id, usuarioId)
-      .subscribe(
-        (valoresPorCategoria) =>
-          (this.valoresPorCategoria = valoresPorCategoria)
-      );
+    this.handlePeriodoData();
   }
 
   getIdUsuarioLogado(): number {
     if (this.usuarioLogado && this.usuarioLogado.id)
       return this.usuarioLogado.id;
-    else throw new Error('usuário não identificado!');
+    else throw new Error('Usuário não identificado!');
   }
 
   getUsuarioLogado() {
     //TODO
     this.usuarioLogado = USUARIOS[1];
+  }
+
+  handlePeriodoData() {
+    const valorTotalPorCategoria$ =
+      this.getTotalPorCategoriaParaOUsuarioNoPeriodo();
+    valorTotalPorCategoria$.subscribe((valoresPorCategoria) => {
+      this.valoresPorCategoria = valoresPorCategoria;
+      this.calcularValorTotal();
+    });
+  }
+
+  getTotalPorCategoriaParaOUsuarioNoPeriodo(): Observable<ValorPorCategoria[]> {
+    let usuarioId = this.getIdUsuarioLogado();
+    if (!this.periodo || !this.periodo.id)
+      throw new Error('Periodo não definido!');
+
+    return this.extratoPorCategoriaService.buscarValorTotalPorCategoriaEUsuarioNoPeriodo(
+      this.periodo.id,
+      usuarioId
+    );
+  }
+
+  calcularValorTotal() {
+    const valorInicial = 0;
+    this.valorTotal = this.valoresPorCategoria
+      .map((cada) => cada.valorTotal)
+      .reduce((acumulador, cadaValor) => acumulador + cadaValor, valorInicial);
   }
 }
