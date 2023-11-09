546211bdddcca3415c731dda6a4e40830fc748a7
luis.gomes <luis.gomes@objective.com.br>
2023-11-09 14:44:31 -0300

    update http interceptor to redirect user to login if its unauthorized

--- src/app/auth/interceptor/auth-interceptor.ts
+++ src/app/auth/interceptor/auth-interceptor.ts
@@ -1,27 +1,46 @@
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) {}
+  constructor(private router: Router, private auth: AuthService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     let authToken = this.auth.getAuthorizationToken();
-    console.log('interceptor token:' + authToken);
-    const authRequest = req.clone({
+    const authRequest = this.addAuthTokenToRequest(req, authToken);
+
+    return next.handle(authRequest).pipe(
+      tap({
+        error: (error: HttpErrorResponse) => {
+          this.handleError(error);
+        },
+      })
+    );
+  }
+
+  private addAuthTokenToRequest(req: HttpRequest<any>, authToken: string) {
+    return req.clone({
       headers: req.headers.set('Authorization', authToken),
     });
+  }
 
-    return next.handle(authRequest);
+  private handleError(error: HttpErrorResponse) {
+    if (this.isUnauthorizedOrForbiden(error)) this.router.navigate(['/login']);
+  }
+
+  private isUnauthorizedOrForbiden(error: HttpErrorResponse) {
+    return error.status === 401 || error.status === 403;
   }
 }
